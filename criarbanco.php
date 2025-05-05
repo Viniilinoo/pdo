bf1e6891b110fe0364e5580c8954b61b838c02b0
unknown <viniciuslino2105@gmail.com>
2025-05-05 20:34:22 -0300

    first commit

---
+++ criarbanco.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Aula de PDO</title>
+</head>
+
+<body>
+
+    <?php
+
+        require_once "config.php";
+
+      try {
+          $conexao = new PDO("mysql:host=$servidor", $usuario, $senha);
+          $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+          $conexao -> exec("CREATE DATABASE livraria");
+          echo "Banco de dados criado com sucesso!";
+      } 
+          catch (PDOException $e) {
+            echo "Não foi possivel criar o Banco de Dados!". $e->getMessage();
+      }
+
+      $conexao = null;
+
+    ?>
+
+</body>
+</html>
---
+++ update.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Aula PDO</title>
+</head>
+
+<body>
+
+    <?php
+    
+    require_once 'config.php';
+
+    try {
+
+        $conexao = new PDO("mysql:host=$servidor;dbname=$db", $usuario, $senha);
+        $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $conexao->exec("UPDATE livros SET autor = 'João Cabral de Melo Neto' WHERE autor = 'Jorge Amado'");
+
+        echo "Atualização realizada com sucesso";
+    } 
+    catch (PDOException $e) {
+        echo "Não foi possível atualizar os Dados.<br>" . $e->getMessage();
+    }
+
+    $conexao = null;
+
+    ?>
+</body>
+
+</html>
